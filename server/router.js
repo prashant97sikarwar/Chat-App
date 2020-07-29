baea1fb44658cced12c1ce8f5f479338d73dfc4b
prashant sikarwar <prashant97sikarwar@gmail.com>
2020-07-29 15:12:01 +0530

    deployment

--- server/router.js
+++ server/router.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 
 router.get('/',  (req, res) => {
-    res.send('Server is up and running');
+    res.send({ response: "Server is up and running."}).status(200);
 });
 
-module.exports = router
+module.exports = router;
